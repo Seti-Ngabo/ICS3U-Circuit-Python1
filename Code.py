128c7ee9beb73c98ba93013e355784c30be5a3a5
Seti-Ngabo <90275806+Seti-Ngabo@users.noreply.github.com>
2021-10-06 13:25:34 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+# Created by Seti Ngabo
+# Created on Sept 2021
+# This is the "Hello, World!" program, with proper style
+
+def main():
+  # this function is the main game_scene
+  
+  print("\n\n\n")
+  print("Hello, World!")
+  
+  # repeat forever, game loop
+  while True:
+    pass # just a placeholder for now
+
+if __name__ == "__main__":
+    main()
